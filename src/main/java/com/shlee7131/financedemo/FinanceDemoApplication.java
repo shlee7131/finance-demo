46e81eef47d2adeec6340c36df5c8787208c0e16
shlee7131 <shlee7131@gmail.com>
2022-05-19 19:13:58 +0900

    first commit

---
+++ src/main/java/com/shlee7131/financedemo/FinanceDemoApplication.java
@@ -0,0 +1,13 @@
+package com.shlee7131.financedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinanceDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shlee7131/financedemo/FinanceDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shlee7131.financedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
